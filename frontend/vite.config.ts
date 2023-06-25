5f5889c68d06a40d2867ed495abdbc0edcf3b7b4
qbornet <ring0.csgo@gmail.com>
2023-06-25 13:04:23 +0200

    modified vite.config added, eslint for vite and used the swc instead of babel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  // so when you do npm run dev no crash occurs just tell you the error.
+  {
+    ...eslint({
+      cache: true,
+      failOnWarning: false,
+      failOnError: false,
+    }),
+    apply: 'serve',
+    enforce: 'post'
+  }
+  ]
 })
