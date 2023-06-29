f8bc7e85a7b2e876c748c648759e27ae9844989d
Quentin BORNET <qbornet@e2r10p1.clusters.42paris.fr>
2023-06-29 15:58:43 +0200

    test config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,40 +1,42 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
     'plugin:prettier/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript'
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'arrow-body-style': 'off',
+    'comma-dangle': 'off',
     'prefer-arrow-callback': 'off',
     'getter-return': 'error',
     'for-direction': 'error',
     'no-const-assign': 'error',
     'no-constructor-return': 'error',
-    'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-  },
+    'react/jsx-uses-react': 'off'
+  }
 };
--- frontend/README.md
+++ frontend/README.md
@@ -4,9 +4,12 @@
 
 ### Unit tests
 
-We are using [Vitest](https://vitest.dev/guide/why.html) to do unit testing of the project. It is close to Jest library but take benefit from the Vite ecosystem.
+We are using [Vitest](https://vitest.dev/guide/why.html) to do unit testing of
+the project. It is close to Jest library but take benefit from the Vite
+ecosystem.
 
-You can start unit testing by running `npm run test` and check test coverage via `npm run coverage`
+You can start unit testing by running `npm run test` and check test coverage via
+`npm run coverage`
 
 ```json
 {
@@ -14,5 +17,11 @@ You can start unit testing by running `npm run test` and check test coverage via
     "test": "vitest",
     "coverage": "vitest run --coverage"
   }
+}
+```
+
+```js
+consttoto=(a)=>{
+  return 123 + a
 }
 ```
