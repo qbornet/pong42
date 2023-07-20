91c393086e2556b2404b61978b69ff84ff0642c1
Julien FRANCAIS <jfrancai@student.42.fr>
2023-07-20 12:32:00 +0200

    adding prettier-plugin-tailwindcss: it auto reorder tailwind styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 function App() {
   return (
     <div>
-      <div className="relative mx-auto bg-gray-800 dark:bg-gray-700 rounded-t-[2.5rem] h-[63px] max-w-[133px]" />
-      <div className="relative mx-auto border-gray-900 dark:bg-gray-800 dark:border-gray-800 border-[10px] rounded-[2.5rem] h-[213px] w-[208px]">
-        <div className="h-[41px] w-[6px] bg-gray-800 dark:bg-gray-800 absolute -right-[16px] top-[40px] rounded-r-lg" />
-        <div className="h-[32px] w-[6px] bg-gray-800 dark:bg-gray-800 absolute -right-[16px] top-[88px] rounded-r-lg" />
-        <div className="rounded-[2rem] overflow-hidden h-[193px] w-[188px]">
+      <div className="relative mx-auto h-[63px] max-w-[133px] rounded-t-[2.5rem] bg-gray-800 dark:bg-gray-700" />
+      <div className="relative mx-auto h-[213px] w-[208px] rounded-[2.5rem] border-[10px] border-gray-900 dark:border-gray-800 dark:bg-gray-800">
+        <div className="absolute -right-[16px] top-[40px] h-[41px] w-[6px] rounded-r-lg bg-gray-800 dark:bg-gray-800" />
+        <div className="absolute -right-[16px] top-[88px] h-[32px] w-[6px] rounded-r-lg bg-gray-800 dark:bg-gray-800" />
+        <div className="h-[193px] w-[188px] overflow-hidden rounded-[2rem]">
           <img
             src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image.png"
-            className="dark:hidden h-[193px] w-[188px"
+            className="w-[188px h-[193px] dark:hidden"
             alt=""
           />
           <img
             src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image-dark.png"
-            className="hidden dark:block h-[193px] w-[188px]"
+            className="hidden h-[193px] w-[188px] dark:block"
             alt=""
           />
         </div>
       </div>
-      <div className="relative mx-auto bg-gray-800 dark:bg-gray-700 rounded-b-[2.5rem] h-[63px] max-w-[133px]" />
+      <div className="relative mx-auto h-[63px] max-w-[133px] rounded-b-[2.5rem] bg-gray-800 dark:bg-gray-700" />
     </div>
   );
 }
