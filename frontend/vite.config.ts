5394da1abbb5eba0d558fc66ed975f1545297ac2
Julien FRANCAIS <jfrancai@student.42.fr>
2023-07-20 12:30:08 +0200

    vite config auto start dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  // so when you do npm run dev no crash occurs just tell you the error.
-  {
-    ...eslint({
-      cache: true,
-      failOnWarning: false,
-      failOnError: false,
-    }),
-    apply: 'serve',
-    enforce: 'post'
-  }
+  server: {
+    open: true
+  },
+  plugins: [
+    react(),
+    // so when you do npm run dev no crash occurs just tell you the error.
+    {
+      ...eslint({
+        cache: true,
+        failOnWarning: false,
+        failOnError: false
+      }),
+      apply: 'serve',
+      enforce: 'post'
+    }
   ]
-})
+});
