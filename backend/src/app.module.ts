cf60a27b92f764682b288f822faca59193227729
Julien FRANCAIS <jfrancai@student.42.fr>
2023-07-24 15:17:15 +0200

    adding module to serve statci spa files inside client folder

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ChatGateway } from './chat/chat.gateway';
 import AppService from './app.service';
 import AppController from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api(.*)']
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, ChatGateway]
 })
 export default class AppModule {}
