13804a8769c15972b4e0b750194bbc20e759f349
Julien FRANCAIS <jfrancai@student.42.fr>
2023-08-15 21:53:38 +0200

    ProfileButton component done

---
+++ frontend/src/components/ProfileButton/ProfileButton.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const styles = Object.freeze({
+  default:
+    'inline-flex items-center justify-center rounded-lg border border-solid border-pong-blue-100 bg-pong-blue-600 px-4 py-1 text-pong-white',
+  clicked:
+    'inline-flex items-center justify-center rounded-lg border border-solid border-pong-blue-100 bg-transparent px-4 py-1 text-pong-blue-100'
+});
+
+function ProfileButton() {
+  const [style, setStyle] = useState<string>(styles.default);
+
+  const handleMouseDown = () => {
+    setStyle(styles.clicked);
+  };
+
+  const handleMouseUp = () => {
+    setStyle(styles.default);
+  };
+
+  return (
+    <button
+      type="button"
+      className={style}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
+      <span className="font-bold ">See Profile</span>
+    </button>
+  );
+}
+
+export default ProfileButton;
