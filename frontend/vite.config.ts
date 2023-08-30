567a746183ec706db51df0317d39b7be52a28cef
Julien FRANCAIS <jfrancai@student.42.fr>
2023-08-30 13:20:41 +0200

    removing auto open vite conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,7 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    open: true
-  },
+  server: {},
   plugins: [
     react(),
     // so when you do npm run dev no crash occurs just tell you the error.
