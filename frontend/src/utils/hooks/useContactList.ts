17338ec0b710846725a027390209ff1037421568
Julien FRANCAIS <jfrancai@student.42.fr>
2023-09-01 15:13:35 +0200

    removing unused files

--- frontend/src/utils/hooks/useContactList.ts
+++
@@ -1,5 +0,0 @@
-import { ContactList } from './useSocket';
-
-export function useContactList(contactList: ContactList) {
-  const [contact, stetContact] = useState<Contact | undefined>(undefined);
-}
